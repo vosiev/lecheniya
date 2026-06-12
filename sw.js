d4e0ccf12a3cd426b171b220045b24a42f2e6bb2
VOSIEV S <vosiev001@gmail.com>
2026-06-12 12:40:10 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'darya-care-v5';
+const ASSETS = [
+  './',
+  './index.html',
+  './assets/css/styles.css',
+  './assets/js/menu-data.js',
+  './assets/js/app.js',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.hostname.includes('api.open-meteo.com')) {
+    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
